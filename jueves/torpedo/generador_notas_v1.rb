486efe09e9d3708acc334c1a924add93a14c3d6c
Karl Heitmann <khf1988@gmail.com>
2021-08-11 22:06:09 -0400

    movido a mi torpedo

---
+++ jueves/torpedo/generador_notas_v1.rb
@@ -0,0 +1,12 @@
+nombre = ARGV[0]
+output_data = File.open "#{nombre}.txt", 'w'
+
+output_data.print("#{nombre}\n")
+# output_data.print(nombre + "\n") # Equivalente a la linea anterior
+
+cantidad_de_notas = rand(7..10)
+
+cantidad_de_notas.times do
+  output_data.puts(rand(10..70) / 10.0)
+  # output_data.puts(rand) # Equivalente a la linea anterior
+end
