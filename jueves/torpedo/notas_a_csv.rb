f94c001a5fe4c8d0c6057683d50cd40f6c87722a
Karl Heitmann <khf1988@gmail.com>
2021-08-11 22:58:00 -0400

    notas_a_csv imprime los datos en orden

--- jueves/torpedo/notas_a_csv.rb
+++ jueves/torpedo/notas_a_csv.rb
@@ -1,3 +1,12 @@
-print ARGV
-puts
-puts ARGV.class
+ARGV.each do |nombre|
+  file_notas = File.open "#{nombre}.txt"
+  lineas = file_notas.readlines
+  print lineas
+  puts
+  puts lineas[0]
+
+  lineas[1..-1].each do |linea|
+    print "> #{linea}"
+  end
+end
+
