97f77fbe7b4bf7585d312205422e63b41fed494d
Karl Heitmann <khf1988@gmail.com>
2021-08-11 22:36:06 -0400

    creados los archivos para comenzar en ejercicio_en_clase

--- jueves/ejercicio_en_clase/generador_notas_v1.rb
+++ jueves/ejercicio_en_clase/generador_notas_v1.rb
@@ -1,7 +1,7 @@
 nombre = ARGV[0]
 output_data = File.open "#{nombre}.txt", 'w'
 
-output_data.print("#{nombre}\n")
+output_data.puts nombre
 # output_data.print(nombre + "\n") # Equivalente a la linea anterior
 
 cantidad_de_notas = rand(7..10)
@@ -9,3 +9,4 @@ cantidad_de_notas = rand(7..10)
 cantidad_de_notas.times do
   output_data.puts(rand(10..70) / 10.0)
 end
+
