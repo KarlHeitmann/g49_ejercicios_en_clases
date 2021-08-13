b85a3045e2bdf9561b15d0d4ee9d32c0dbfea4e6
Karl Heitmann <khf1988@gmail.com>
2021-08-12 20:21:49 -0400

    un misterio el gets.chomp de ejercicio_en_clase/notas_a_csv

--- jueves/ejercicio_en_clase/notas_a_csv.rb
+++ jueves/ejercicio_en_clase/notas_a_csv.rb
@@ -1,3 +1,25 @@
 print ARGV
 puts
 puts ARGV.class
+
+recordatorio = gets.chomp
+puts "recordatorio: #{recordatorio}"
+archivo_promedios = File.new "promedio.csv", "w"
+archivo_promedios.puts "nombre,promedio"
+
+i = 0
+while i < ARGV.length
+  nombre = ARGV[i]
+  archivo_notas = File.new "#{nombre}.txt"
+  lineas = archivo_notas.readlines
+  nombre_persona = lineas[0]
+  puts "nombre_persona: #{nombre_persona}"
+  print "#{lineas}"
+  puts
+  notas = lineas[1..-1]
+  puts "las notas de las persona fueron: #{notas}"
+  promedio = notas.reduce(0.0) {|sum, nota| sum + nota.to_f} / notas.length
+  puts "el promedio es: #{promedio}"
+  archivo_promedios.puts "#{nombre_persona},#{promedio}"
+  i = i + 1
+end
