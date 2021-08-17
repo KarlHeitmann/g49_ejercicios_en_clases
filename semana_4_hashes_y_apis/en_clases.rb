f511b0288bf5d28934ab428a3a6b5088b5688bd9
Karl Heitmann <khf1988@gmail.com>
2021-08-17 18:43:46 -0400

    agregado archivo para trabajar en clases

---
+++ semana_4_hashes_y_apis/en_clases.rb
@@ -0,0 +1,6 @@
+# Tome los datos de la API de Coingecko.
+# 1. Tome las criptomonedas "calientes" del coingecko, las que están en tendencia. Saque los datos de este endpoint de la API:
+# /search/trending que le proporcionará las TOP7 monedas que más han sido buscadas por los usuarios de coingecjo en las últimas 24 horas.
+# Guarde el ID de cada una de las criptomonedas y su "market_cap_rank" en un archivo .csv
+# 2. OPCIONAL: Para cada moneda, busque la información de su precio actual, y agreguelo en el archivo .csv como una columna más
+#
