fda2e3107f5bd5c07e1c2e27b0302530c3c6d203
Karl Heitmann <khf1988@gmail.com>
2021-08-16 22:46:08 -0400

    imprime los datos en un archivo de texto

--- semana_4_hashes_y_apis/api_crypto.rb
+++ semana_4_hashes_y_apis/api_crypto.rb
@@ -10,4 +10,16 @@ https.use_ssl = true
 request = Net::HTTP::Get.new(url)
 
 response = https.request(request)
-puts JSON.parse(response.read_body)
+datos = JSON.parse(response.read_body)
+
+puts datos
+archivo = File.open "tendencias.csv", "w"
+
+archivo.puts "id,market_cap_rank"
+
+datos['coins'].each do |dato|
+  dato['item']
+  archivo.puts "#{dato['item']['id']},#{dato['item']['market_cap_rank']}"
+end
+
+archivo.close
