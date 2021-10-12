0a8206f151306aec9b51b754fd817be291209bb3
Karl Heitmann <khf1988@gmail.com>
2021-10-12 17:17:19 -0300

    comprueba que ya se haya sacado una carta antes

--- 04_rails/semana_02/baraja_especial.rb
+++ 04_rails/semana_02/baraja_especial.rb
@@ -1,15 +1,15 @@
 cartas_sacar = ARGV[0].to_i
 puts cartas_sacar
 class BarajaEspanola
-  CARTAS_TOTALES = 40
   PINTAS_POSIBLES = ['oros', 'copas', 'espadas', 'bastos']
   NUMEROS_POSIBLE = [1,2,3,4,5,6,7,10,11,12]
-  @@cartas = 0
+  @@cartas = []
   def initialize
-    raise "Error: ya se han sacado todas las cartas" unless @@cartas < CARTAS_TOTALES
-    @@cartas += 1
     @pinta = PINTAS_POSIBLES.sample
     @numero = NUMEROS_POSIBLE.sample
+    carta_sacada = {pinta: @pinta, numero: @numero}
+    raise "Error: ya se ha sacado esta carta" if @@cartas.include? carta_sacada
+    @@cartas << carta_sacada
   end
 
   def to_s
