7d2a2eb7582e53e028d7f8d6df252d91399b5c46
Karl Heitmann <khf1988@gmail.com>
2021-10-12 17:11:19 -0300

    mas orden

--- 04_rails/semana_02/baraja_especial.rb
+++ 04_rails/semana_02/baraja_especial.rb
@@ -1,11 +1,15 @@
+cartas_sacar = ARGV[0].to_i
+puts cartas_sacar
 class BarajaEspanola
-  CARTAS_TOTALES = 8
+  CARTAS_TOTALES = 40
+  PINTAS_POSIBLES = ['oros', 'copas', 'espadas', 'bastos']
+  NUMEROS_POSIBLE = [1,2,3,4,5,6,7,10,11,12]
   @@cartas = 0
   def initialize
     raise "Error: ya se han sacado todas las cartas" unless @@cartas < CARTAS_TOTALES
     @@cartas += 1
-    @pinta = ['oros', 'copas', 'espadas', 'bastos'].sample
-    @numero = [1,2].sample
+    @pinta = PINTAS_POSIBLES.sample
+    @numero = NUMEROS_POSIBLE.sample
   end
 
   def to_s
@@ -13,4 +17,4 @@ class BarajaEspanola
   end
 end
 
-8.times {|i| puts BarajaEspanola.new }
+cartas_sacar.times {|i| puts BarajaEspanola.new }
