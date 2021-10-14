07afb0b4bcb868635f3babd32f574b7646c6542b
Karl Heitmann <khf1988@gmail.com>
2021-10-14 18:53:16 -0300

    agregado inicio ejercicio en clases

---
+++ 04_rails/semana_02/ejercicio/carta_espanola_test.rb
@@ -0,0 +1,11 @@
+require 'test/unit'
+require_relative 'carta_espanola'
+
+# Documentacion: https://docs.ruby-lang.org/en/2.1.0/MiniTest/Assertions.html
+
+class TestCartaEspanola < Test::Unit::TestCase
+    def test_method_to_s
+      carta = CartaEspanola.new('espadas', 10)
+      assert_equal(carta.to_s, "10 - espadas")
+    end
+end
