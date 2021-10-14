0e7c5d39c9644e21767a908474f4cc98d73a0066
Karl Heitmann <khf1988@gmail.com>
2021-10-14 18:50:51 -0300

    listo el ejemplo

--- 04_rails/semana_02/solucion_jueves/carta_inglesa.rb
+++ 04_rails/semana_02/solucion_jueves/carta_inglesa.rb
@@ -1,10 +1,51 @@
 require_relative 'carta'
+require_relative 'jugar'
 
 class CartaInglesa < Carta
+  include Jugar
+  attr_accessor :pinta, :numero
   def initialize(numero, pinta)
     @pintas_posibles = ['picas', 'trebol', 'corazon', 'diamante']
-    @numeros_posibles = [1,2,3,4,5,6,7,8,9,10,11,12]
+    @numeros_posibles = ['as',2,3,4,5,6,7,8,9,10,11,12]
     super(numero, pinta)
+  end
+
+  def poker_g49(oponente)
+    if oponente.numero < @numero
+      return 'ganador'
+    elsif oponente.numero > @numero
+      return 'perdedor'
+    else
+      if @pinta == 'picas'
+        if oponente.pinta == 'picas'
+          return 'empate'
+        else
+          return 'ganador'
+        end
+      elsif @pinta == 'trebol'
+        if oponente.pinta == 'picas'
+          return 'perdedor'
+        elsif oponente.pinta == 'trebol'
+          return 'empate'
+        else
+          return 'ganador'
+        end
+      elsif @pinta == 'corazon'
+        if oponente.pinta == 'diamante'
+          return 'ganador'
+        elsif oponente.pinta == 'corazon'
+          return 'empate'
+        else
+          return 'perdedor'
+        end
+      elsif @pinta == 'diamante'
+        if oponente.pinta == 'diamante'
+          return 'empate'
+        else
+          return 'perdedor'
+        end
+      end
+    end
   end
 end
 
