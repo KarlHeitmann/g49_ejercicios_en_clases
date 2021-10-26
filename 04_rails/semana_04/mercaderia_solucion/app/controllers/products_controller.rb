d923e28e1c7d82dc129b49145be5176791881f44
Karl Heitmann <khf1988@gmail.com>
2021-10-26 19:03:15 -0300

    listo con controlador

---
+++ 04_rails/semana_04/mercaderia_solucion/app/controllers/products_controller.rb
@@ -0,0 +1,24 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:success] = "Producto creado exitosamente"
+    else
+      flash[:danger] = "Errores al crear el producto"
+    end
+    redirect_to products__path
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:nombre, :descripcion, :codigo_de_barra)
+    end
+end
