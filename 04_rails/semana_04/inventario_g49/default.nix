02cf6e222c6fa142966466fbef06f8c7e567b24c
Karl Heitmann <khf1988@gmail.com>
2021-11-07 16:32:47 -0300

    agregado archivos de nix

---
+++ 04_rails/semana_04/inventario_g49/default.nix
@@ -0,0 +1,12 @@
+with (import <nixpkgs> {});
+let
+  gems = bundlerEnv {
+    name = "your-package";
+    inherit ruby;
+    gemdir = ./.;
+  };
+in stdenv.mkDerivation {
+  name = "your-package";
+  buildInputs = [gems ruby];
+}
+
