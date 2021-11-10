f19be739598d2f18b6341c0ddf2f23d8e8d2e449
Karl Heitmann <khf1988@gmail.com>
2021-11-09 21:39:18 -0300

    agregada la relacion 1-n una categoria tiene muchas peliculas

--- 04_rails/semana_05/http/clase_peliculas/db/schema.rb
+++ 04_rails/semana_05/http/clase_peliculas/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_223443) do
+ActiveRecord::Schema.define(version: 2021_11_09_232422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 2021_11_09_223443) do
     t.date "lanzamiento"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_movies_on_category_id"
   end
 
+  add_foreign_key "movies", "categories"
 end
