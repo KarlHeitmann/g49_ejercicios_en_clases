343764b87cbae93e7871744e4134dddf8bf1df65
Karl Heitmann <karlheitmann@Karls-MacBook-Pro.local>
2021-11-18 19:40:54 -0300

    refactorizacion y un poco mas de orden en proyecto peliculas

--- 04_rails/semana_05/http/clase_peliculas/app/controllers/movies_controller.rb
+++ 04_rails/semana_05/http/clase_peliculas/app/controllers/movies_controller.rb
@@ -1,30 +1,27 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: %i[ show edit update destroy ]
+  before_action :set_category
 
   def test
   end
 
   # GET /movies or /movies.json
   def index
-    @category = Category.find params[:category_id]
     @movies = @category.movies
   end
 
   # GET /movies/1 or /movies/1.json
   def show
-    @category = Category.find params[:category_id]
   end
 
   # GET /movies/new
   def new
     @movie = Movie.new
-    @category = Category.find(params[:category_id])
     @categories = Category.pluck :name, :id
   end
 
   # GET /movies/1/edit
   def edit
-    @category = Category.find params[:category_id]
   end
 
   # POST /movies or /movies.json
@@ -33,7 +30,6 @@ class MoviesController < ApplicationController
     puts params[:movie]
     puts "============================"
     @movie = Movie.new(movie_params)
-    @category = Category.find params[:category_id]
     @movie.category_id = @category.id
     # @movie.category_id = params[:category]
     respond_to do |format|
@@ -50,7 +46,6 @@ class MoviesController < ApplicationController
 
   # PATCH/PUT /movies/1 or /movies/1.json
   def update
-    @category = Category.find params[:category_id]
     respond_to do |format|
       if @movie.update(movie_params)
         format.html { redirect_to category_movie_path(@category.id, @movie), notice: "Movie was successfully created." }
@@ -65,7 +60,6 @@ class MoviesController < ApplicationController
   # DELETE /movies/1 or /movies/1.json
   def destroy
     @movie.destroy
-    @category = Category.find params[:category_id]
     respond_to do |format|
       format.html { redirect_to category_movies_path(@category.id), notice: "Movie was successfully destroyed." }
       format.json { head :no_content }
@@ -77,6 +71,10 @@ class MoviesController < ApplicationController
     # DRY = Don't Repeat Yourself. DRY = SECO
     def set_movie
       @movie = Movie.find(params[:id])
+    end
+
+    def set_category
+      @category = Category.find(params[:category_id])
     end
 
     # Only allow a list of trusted parameters through.
