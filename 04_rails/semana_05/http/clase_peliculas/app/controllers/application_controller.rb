bee90614a5446a2d2cddac8de1e4eccd844fc9bf
Karl Heitmann <karlheitmann@Karls-MacBook-Pro.local>
2021-11-18 21:13:44 -0300

    generadas las vistas de devise y agregado los parametros adicionales de usuario en el formulario de registro

--- 04_rails/semana_05/http/clase_peliculas/app/controllers/application_controller.rb
+++ 04_rails/semana_05/http/clase_peliculas/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller? # HACK revise esto: https://github.com/heartcombo/devise#strong-parameters 
+
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nombre, :apellidos, :telefono])
+  end
 end
