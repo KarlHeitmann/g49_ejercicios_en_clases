4d6b5c69b40f88bcfb9ae6e99bb0252afc881af6
Karl Heitmann <karlheitmann@Karls-MacBook-Pro.local>
2021-11-23 20:20:29 -0300

    agregado comentario

--- 04_rails/semana_05/http/clase_peliculas/db/seeds.rb
+++ 04_rails/semana_05/http/clase_peliculas/db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# HACK uso recomendado con el comando `rails db:reset`. Use con precaución porque lo borra todo.
+
 User.create!(email: "user1@example.com", password: "asdasd", nombre: "user 1", apellidos: "apellido 1", telefono: "+56 9 1122 3344")
 User.create!(email: "user2@example.com", password: "asdasd", nombre: "user 2", apellidos: "apellido 2", telefono: "+56 9 1122 3345")
 User.create!(email: "user3@example.com", password: "asdasd", nombre: "user 3", apellidos: "apellido 3", telefono: "+56 9 1122 3346")
