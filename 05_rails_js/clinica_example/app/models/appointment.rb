dfe6aafb8b1d86d3a691af67bbc6d32d62421225
Karl Heitmann <karlheitmann@Karls-MacBook-Pro.local>
2022-01-06 20:41:38 -0300

    terminados los scopes

--- 05_rails_js/clinica_example/app/models/appointment.rb
+++ 05_rails_js/clinica_example/app/models/appointment.rb
@@ -1,4 +1,7 @@
 class Appointment < ApplicationRecord
+  scope :futures, -> { where("appointment_date > ?", DateTime.now) }
+  scope :pasts, -> { where("appointment_date < ?", DateTime.now) }
+
   belongs_to :physician
   belongs_to :patient
 end
