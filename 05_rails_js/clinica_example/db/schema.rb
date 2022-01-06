e0aedb7dccb0a25c7675bb4913b89a44f6fb1fb9
Karl Heitmann <karlheitmann@Karls-MacBook-Pro.local>
2022-01-06 14:38:26 -0300

    pacientes scaffold OK

--- 05_rails_js/clinica_example/db/schema.rb
+++ 05_rails_js/clinica_example/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_06_164746) do
+ActiveRecord::Schema.define(version: 2022_01_06_173157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "physicians", force: :cascade do |t|
     t.string "name"
