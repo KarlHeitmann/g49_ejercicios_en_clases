b84a66512113ce3c003372b32ab2d77b7dac41f5
Karl Heitmann <khf1988@gmail.com>
2022-04-05 19:59:54 -0400

    enriquecido los seeds

--- 05_rails_js/clinica_example/app/models/physician.rb
+++ 05_rails_js/clinica_example/app/models/physician.rb
@@ -1,4 +1,5 @@
 class Physician < ApplicationRecord
+  #has_many :appointments, dependent: :destroy_all # TAREA: Revisar el dependent destroy_all, y como influiría con Physician.destroy_all
   has_many :appointments
   has_many :patients, through: :appointments
   validates :name, :speciality, presence: true
--- 05_rails_js/clinica_example/db/seeds.rb
+++ 05_rails_js/clinica_example/db/seeds.rb
@@ -6,8 +6,47 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Physician.create(name: "Gregory House", speciality: "Nefrologia y enfermedades infecciosas")
-Physician.create(name: "Chapatín", speciality: "Médico general")
+# A este tipo de archivos, que se leen de arriba hacia abajo
+# se les llama ::::SCRIPT::::
 
-Patient.create(name: "Jane Doe")
-Patient.create(name: "John Doe")
+# medicos = []
+# 10.times do |i|
+#   medicos << Physician.create(name: Faker, speciality: Faker )# TAREA: usar faker, porque esto no funciona así como está
+# end
+
+=begin
+# ATENCION: Ocupar esto solo si yo voy a ocupar el comando rails db:seed. Si ocupo rails db:seed
+# ES IMPERATIVO limpiar los datos.
+puts "Inicio LIMPIEZA"
+
+# HACK Y TAREA: Investigar sobre las rake tasks
+
+Appointment.destroy_all
+Physician.destroy_all
+Patient.destroy_all
+
+puts "FIN DE LIMPIEZA"
+=end
+
+def crear_10_medicos
+  medicos = []
+  10.times do |i|
+    medicos << Physician.create(name: Faker, speciality: Faker )# TAREA: usar faker, porque esto no funciona así como está
+  end
+  return medicos
+end
+
+house = Physician.create!(name: "Gregory House", speciality: "Nefrologia y enfermedades infecciosas")
+chapatin = Physician.create!(name: "Chapatín", speciality: "Médico general")
+
+puts "Doctores creados"
+
+jane = Patient.create!(name: "Jane Doe")
+john = Patient.create!(name: "John Doe")
+
+puts "Pacientes creados"
+
+appointment1 = Appointment.create!(physician: house, patient: jane, appointment_date: 1.day.from_now)
+appointment2 = Appointment.create!(physician_id: house.id, patient_id: jane.id, appointment_date: 1.year.from_now)
+
+puts ":::::::::::::::"
